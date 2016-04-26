883c16e500402622759e851c1b5ed12cbe256d4a
HariKvana <harikvana@gmail.com>
2016-04-26 12:04:39 +0530

    removed git

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  match 'operations' => 'operations#operations', via: [:get, :post]
-  
+  match 'operations' => 'operations#operations', via: [:post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
